30f6809482a64e29e6dbb158dcbd05e6cde824ea
ykaragol <>
2015-05-20 23:15:23 +0300

    first try

--- YemekMenu/src/yemekmenu/parser/Parser.java
+++ YemekMenu/src/yemekmenu/parser/Parser.java
@@ -23,10 +23,10 @@ public class Parser {
 	public List<GununYemegi> aylikYemekleriCek() throws IOException {
 		List<GununYemegi> ayinYemegi = new LinkedList<GununYemegi>();
 		Document doc = Jsoup.connect(ADDRESS).get();
-		Elements headlines = doc.select("table tr td table tr");
+		Elements headlines = doc.select("table tr td table tr[height=30]");
 		for (int i = 1; i < headlines.size(); i++) {
 			Element dayRow = headlines.get(i);
-			Elements dayCells = dayRow.getElementsByTag("td");
+			Elements dayCells = dayRow.select("> td");
 			Element day = dayCells.get(0);
 			DateFormat instance = new SimpleDateFormat("dd MMM yyyy EEE", new Locale("tr", "TR"));
 			GununYemegi g = new GununYemegi();
@@ -37,10 +37,26 @@ public class Parser {
 				// pass
 				continue;
 			}
-			String[] meals = new String[dayCells.size()];
+			String[] meals = new String[dayCells.size()-1];
 			for (int j = 1; j < dayCells.size(); j++) {
 				Element element = dayCells.get(j);
-				meals[j - 1] = element.text();
+				
+				Elements innerTable = element.select("table tr td");
+				if(innerTable.size() == 0){				
+					meals[j-1] = element.text();
+				}else{
+					StringBuilder builder = new StringBuilder();
+					builder.append(innerTable.get(0).text());
+					for(int k=1; k<innerTable.size(); k++){
+						String text = innerTable.get(k).text();
+						text = text.replace("\u00A0"," ").replaceAll("&nbsp;", "").trim();
+						if(text.length()>0){
+							builder.append(" / ");
+							builder.append(text);
+						}
+					}
+					meals[j-1] = builder.toString();
+				}
 			}
 			g.setMeal(meals);
 			ayinYemegi.add(g);
