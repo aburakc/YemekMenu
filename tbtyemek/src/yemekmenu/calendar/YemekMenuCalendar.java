ff1739266ed0bf53c1e6532fb7870df38c3d1bf9
Burak <Burak@10.222.100.20>
2015-05-21 13:39:29 +0300

    app engine

---
+++ tbtyemek/src/yemekmenu/calendar/YemekMenuCalendar.java
@@ -0,0 +1,62 @@
+package yemekmenu.calendar;
+
+import java.util.Date;
+
+import net.fortuna.ical4j.model.Calendar;
+import net.fortuna.ical4j.model.Property;
+import net.fortuna.ical4j.model.component.VEvent;
+import net.fortuna.ical4j.model.parameter.Value;
+import net.fortuna.ical4j.model.property.Description;
+import net.fortuna.ical4j.model.property.Uid;
+import net.fortuna.ical4j.util.CompatibilityHints;
+
+public class YemekMenuCalendar {
+	private static Calendar calendar;
+
+	public static Calendar getInstance() {
+		if (getCalendar() == null){
+			setCalendar(new Calendar());
+			CompatibilityHints.setHintEnabled("KEY_RELAXED_UNFOLDING", true);
+		}
+
+		return getCalendar();
+	}
+
+	public static void cleanCalendar(){
+		getInstance().getComponents().clear();
+	}
+	
+	public static void addMenu(Date date, String[] menu) {
+		java.util.Calendar newCal = java.util.Calendar.getInstance();
+		newCal.setTime(date);
+		//newCal.add(java.util.Calendar.DAY_OF_MONTH, 1);
+		String menuName = "TÜBİTAK YEMEK MÖNÜSÜ";
+		if(menu != null && menu.length > 0){
+			menuName = menu[0];
+		}
+		VEvent menuEvent = new VEvent(new net.fortuna.ical4j.model.Date(newCal.getTime()),
+				menuName);
+		menuEvent.getProperties().getProperty(Property.DTSTART).getParameters()
+				.add(Value.DATE);
+
+		Uid uid = new Uid(date.getTime() + "TUBITAK");
+		menuEvent.getProperties().add(uid);
+		StringBuilder sb = new StringBuilder();
+		for (String str : menu) {
+			sb.append(str);
+			sb.append("\r\n");
+		}
+		Description description = new Description(sb.toString());
+		menuEvent.getProperties().add(description);
+		getInstance().getComponents().add(menuEvent);
+	}
+
+	public static Calendar getCalendar() {
+		return calendar;
+	}
+
+	public static void setCalendar(Calendar calendar) {
+		YemekMenuCalendar.calendar = calendar;
+	}
+
+}
---
+++ tbtyemek/src/yemekmenu/servlet/iCal.java
@@ -0,0 +1,60 @@
+package yemekmenu.servlet;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Calendar;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import yemekmenu.calendar.YemekMenuCalendar;
+import yemekmenu.domain.GununYemegi;
+import yemekmenu.parser.Parser;
+
+
+public class iCal extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static long lastUpdateTime = 0;
+
+	public iCal() {
+		super();
+	}
+
+	private void getMenu(){
+		Parser p = new Parser();
+		try {
+			YemekMenuCalendar.cleanCalendar();
+			List<GununYemegi> ayinYemegi = p.aylikYemekleriCek();
+			for(GununYemegi gy : ayinYemegi){
+				YemekMenuCalendar.addMenu(gy.getDay(), gy.getMeal());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		long currentTimeMillis = System.currentTimeMillis();
+		if (lastUpdateTime == 0 || (currentTimeMillis-lastUpdateTime)>(1000*60*60) ) {
+			getMenu();
+			lastUpdateTime = currentTimeMillis;
+		}
+		response.setCharacterEncoding("UTF-8");
+		response.getWriter().write(YemekMenuCalendar.getInstance().toString());
+	}
+
+
+
+}
